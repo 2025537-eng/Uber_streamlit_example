ec392e23c6a33f1cf299bcdc876196502a4e2e7a
2025537-eng <2025537@student.cct.ie>
2025-12-11 13:37:00 -0800

    map1

--- uber.py
+++ uber.py
@@ -31,3 +31,9 @@ hist_values=np.histogram(data[DATE_COLUMN].dt.hour,
 			bins=24, range=(0,24))[0]
 
 st.bar_chart(hist_values)
+
+hour_to_filter=st.slider("hour",0,23,17)
+filtered_data=data[data[DATE_COLUMN].dt.hour == hour_to_filter ]
+
+st.subheader("MAP of all pickups at %s:00" % hour_to_filter )
+st.map(filtered_data)
